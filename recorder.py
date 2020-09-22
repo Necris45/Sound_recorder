a330fcc0b852425b6f8260d8bb5b815ab605220b
Necris45 <Necris01@mail.ru>
2020-09-22 20:25:33 +0500

    примерная логика работы

--- recorder.py
+++ recorder.py
@@ -8,9 +8,62 @@ chunk = 1024  # Запись кусками по 1024 сэмпла
 sample_format = pyaudio.paInt16  # 16 бит на выборку
 channels = 2
 rate = 44100  # Запись со скоростью 44100 выборок(samples) в секунду
-seconds = 2048
 filename1 = "input_sound.wav"
 filename2 = "output_sound.wav"
+p = pyaudio.PyAudio()  # Создать интерфейс для PortAudio
+y = 0
+
+
+def record():
+    y += 1
+    stream1 = p.open(format=sample_format,
+                     channels=channels,
+                     rate=rate,
+                     frames_per_buffer=chunk,
+                     input_device_index=2,  # индекс устройства с которого будет идти запись звука (микрофон)
+                     input=True)
+    stream2 = p.open(format=sample_format,
+                     channels=channels,
+                     rate=rate,
+                     frames_per_buffer=chunk,
+                     input_device_index=1,  # индекс устройства с которого будет идти запись (стереомикшер)
+                     input=True)
+    frames1 = []  # Инициализировать массив для хранения данных с микрофона
+    frames2 = []  # Инициализировать массив для хранения данных с стереомикшера
+    for i in range(0, 88200):
+        while y > 0:
+            data1 = stream1.read(chunk)
+            frames1.append(data1)
+            data2 = stream2.read(chunk)
+            frames2.append(data2)
+    if y > 0:
+        y -=1
+    # Остановить и закрыть поток
+    stream1.stop_stream()
+    stream2.stop_stream()
+    stream1.close()
+    stream2.close()
+    # Завершить интерфейс PortAudio
+    p.terminate()
+    # Сохранить записанные данные в виде файла wav
+    wf1 = wave.open(filename1, 'wb')
+    wf1.setnchannels(channels)
+    wf1.setsampwidth(p.get_sample_size(sample_format))
+    wf1.setframerate(rate)
+    wf1.writeframes(b''.join(frames1))
+    wf1.close()
+    if len(frames2) > 0:
+        wf2 = wave.open(filename2, 'wb')
+        wf2.setnchannels(channels)
+        wf2.setsampwidth(p.get_sample_size(sample_format))
+        wf2.setframerate(rate)
+        wf2.writeframes(b''.join(frames2))
+        wf2.close()
+    # Объединяем имеющиеся wav файлы в единый mp3 файл
+    sound1 = AudioSegment.from_file("input_sound.wav", format="wav")
+    sound2 = AudioSegment.from_file("output_sound.wav", format="wav")
+    sound = sound1.overlay(sound2)
+    sound.export("mixin.mp3", format='mp3')
 
 
 class Recorder(QWidget):
@@ -29,65 +82,8 @@ class Recorder(QWidget):
         self.button2 = QPushButton(self)
         self.button2.setGeometry(233, 0, 50, 50)
         self.button2.setText('Стоп')
-        self.button1.clicked.connect(self.record)
+        self.button1.clicked.connect(record())
         self.button2.clicked.connect(self.stop_record)
 
-    def stop_record():
-        # Остановить и закрыть поток
-        stream1.stop_stream()
-        stream2.stop_stream()
-        stream1.close()
-        stream2.close()
-        # Завершить интерфейс PortAudio
-        p.terminate()
-        # Сохранить записанные данные в виде файла wav
-        wf1 = wave.open(filename1, 'wb')
-        wf1.setnchannels(channels)
-        wf1.setsampwidth(p.get_sample_size(sample_format))
-        wf1.setframerate(rate)
-        wf1.writeframes(b''.join(frames1))
-        wf1.close()
-        if len(frames2) > 0:
-            wf2 = wave.open(filename2, 'wb')
-            wf2.setnchannels(channels)
-            wf2.setsampwidth(p.get_sample_size(sample_format))
-            wf2.setframerate(rate)
-            wf2.writeframes(b''.join(frames2))
-            wf2.close()
-        # Объединяем имеющиеся wav файлы в единый mp3 файл
-        sound1 = AudioSegment.from_file("input_sound.wav", format="wav")
-        sound2 = AudioSegment.from_file("output_sound.wav", format="wav")
-        sound = sound1.overlay(sound2)
-        sound.export("mixin.mp3", format='mp3')
-
-    def record():
-        p = pyaudio.PyAudio()  # Создать интерфейс для PortAudio
-        stream1 = p.open(format=sample_format,
-                         channels=channels,
-                         rate=rate,
-                         frames_per_buffer=chunk,
-                         input_device_index=2,  # индекс устройства с которого будет идти запись звука (микрофон)
-                         input=True)
-        if self.drop1.currentText() != "Запись только с микрофона":
-            stream2 = p.open(format=sample_format,
-                             channels=channels,
-                             rate=rate,
-                             frames_per_buffer=chunk,
-                             input_device_index=1,  # индекс устройства с которого будет идти запись (стереомикшер)
-                             input=True)
-        frames1 = []  # Инициализировать массив для хранения данных с микрофона
-        frames2 = []  # Инициализировать массив для хранения данных с стереомикшера
-        if self.drop1.currentText() != "Запись только с микрофона":
-            for i in range(0, int(rate / chunk * seconds)):
-                data1 = stream1.read(chunk)
-                frames1.append(data1)
-                data2 = stream2.read(chunk)
-                frames2.append(data2)
-                if i >= 88200:
-                    stop_record()
-        else:
-            for i in range(0, int(rate / chunk * seconds)):
-                data1 = stream1.read(chunk)
-                frames1.append(data1)
-                if i >= 88200:
-                    stop_record()
+    def stop_record(self):
+        y -=1
