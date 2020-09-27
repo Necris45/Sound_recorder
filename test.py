1d281a8789d40713796a4861ecee3f892a734188
Necris45 <Necris01@mail.ru>
2020-09-27 22:37:25 +0500

    Закончил переименование переменных. Все работает стабильно

--- test.py
+++
@@ -1,6 +0,0 @@
-from pydub import AudioSegment
-
-sound1 = AudioSegment.from_file("input_sound.wav", format="wav")
-sound2 = AudioSegment.from_file("output_sound.wav", format="wav")
-sound = sound1 + sound2
-comfile_handle = sound.export("output.mp3", format="mp3")
